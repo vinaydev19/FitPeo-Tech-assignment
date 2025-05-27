237a229467028f4709f9f6e5a46cd6cea1bf0296
vinay1945 <vinaykumarporandla19@gmail.com>
2025-05-27 17:11:33 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import Header from "./components/Header"
 import './App.css'
 import DashboardMainContent from './components/DashboardMainContent'
 import SideMenu from "./components/SideMenu"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="min-h-screen bg-gray-50">
