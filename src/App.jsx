d195cf346b33ae7721f8a6571be785fc18296d06
vinay1945 <vinaykumarporandla19@gmail.com>
2025-05-27 11:34:24 +0530

    fix the deployment problem

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from "./components/Header"
 import './App.css'
-import SideBar from './components/sidebar'
 import DashboardMainContent from './components/DashboardMainContent'
+import SideBar from './components/sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
